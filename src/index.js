b11e92f9ad5c31002152c3414d7fb9b118353111
eng.sayyar <eng.sayyar@gmail.com>
2020-05-19 00:28:55 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+class  FetchUser extends React.Component{
+ 
+  state=
+  {
+    loding:true,
+    people:[],
+  };
+
+async componentDidMount(){
+  const url="https://api.randomuser.me/?results=10";
+  const response=await fetch(url);
+  const data=await response.json();
+  //this.setState({person:JSON.stringify(data)})
+  this.setState({people:data.results,loding:false});
+
+}
+
+render()
+{
+if(this.state.loding)
+{
+return <div>loding...</div>
+}
+if(!this.state.people.length)
+{
+return <div>did not get person!</div>
+}
+return (
+
+<div style={{color:'blue'}}>
+  {this.state.people.map(person=>(
+    <div key={person.login.uuid}>
+    <img src={person.picture.large} alt={person.login.username}></img>
+   <div>Title:{person.name.title}</div>
+   <div>first:{person.name.first}</div>
+   <div>last:{person.name.last}</div>
+   <div>phone:{person.phone}</div>
+   <div>email:{person.email}</div>
+   </div>
+  ))}
+ 
+
+
+   
+</div>
+);
+}
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <FetchUser />,
+
+document.getElementById('root')
+);
